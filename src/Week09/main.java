416e6aa379f133e9e371e65ebeedf79af1482f67
Joshua Simangunsong <youremail@example.com>
2025-11-11 21:06:43 +0700

    Class Car implements Vehicle

---
+++ src/Week09/main.java
@@ -0,0 +1,11 @@
+package Week09;
+
+public class main {
+	public static void main(String[] args) {
+		Car myCar = new Car("Honda Civic");
+		myCar.showVehicleInfo();
+		myCar.start();
+		myCar.stop();
+	}
+
+}
